26748a686d031fbe55addb71e4424e9bfec8e314
sunita <sunitakwr21@gmail.com>
2017-12-10 17:20:29 +0545

    "acceleration"

---
+++ acceleration.py
@@ -0,0 +1,14 @@
+'''We have v=u+at where v is velocity, u is initial velocity, and t is time.
+ And value for each is as follows: v=25m/s. u=0m/s, t=10 seconds. Find the acceleration a'''
+
+def fun(v,u,t):
+
+    return (a)
+v = 25
+u = 0
+t = 10
+a=((v-u)/t)
+
+
+
+print("a={}".format(a))
--- passwordvalue.py
+++ passwordvalue.py
@@ -1,3 +1,8 @@
+'''Define a global static password value by yourself.
+ Then define a function which prompts user for a password.
+ If the password is a match, print "password match" or else print "Wrong password"'''
+
+
 class Const():
     def password():
         return 1
