7604cd8961ff199cb56e05c3c9eebbd110f2cbb4
sunita <sunitakwr21@gmail.com>
2017-12-11 09:50:44 +0545

    "password"

---
+++ PASSWORD.py
@@ -0,0 +1,14 @@
+'''Define a global static password value by yourself.
+ Then define a function which prompts user for a password.
+ If the password is a match, print "password match" or else print "Wrong password"'''
+
+
+password="rock"
+def check(password):
+    ans=input("enter the password:")
+    if (ans==password):
+        print("password match")
+    else:
+        print("wrong password")
+
+check(password)
